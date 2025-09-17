9d322635b2f5481881ff33d5a9baee4aa99fcb84
ShivuJ <shivanijadhav79@gmail.com>
2025-09-17 21:03:50 +0530

    Date Function - completed

--- Jarvis/commands.py
+++ Jarvis/commands.py
@@ -3,8 +3,9 @@ import datetime
 from speech import speak
 
 def date():
-    dateNow = datetime.datetime.now()
-    speak(f"The current date is {dateNow}")
+    datetoday = datetime.date.today()
+    formatted_date = datetoday.strftime("%B %d, %Y")
+    speak(f"The current date is {formatted_date}")
 
 def wish():
     speak("Welcome back, How may I help you?")
